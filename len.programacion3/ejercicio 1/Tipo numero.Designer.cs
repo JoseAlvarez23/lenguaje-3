1d1e3c01ac90543cd87b2d471a859f45e6b13fcb
JoseAlvarez23 <jralvarezl@unah.hn>
2021-09-16 23:54:22 -0600

    Tarea 1
    Tarea 1

---
+++ len.programacion3/ejercicio 1/Tipo numero.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_tipo = new System.Windows.Forms.TextBox();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.txt_primo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(171, 106);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(97, 48);
+            this.btn_calcular.TabIndex = 0;
+            this.btn_calcular.Text = "CALCULAR";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IDENTIFICAR QUE TIPO DE NUMERO ES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "INGRESE EL NUMERO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "EL NUMERO ES PAR O IMPAR:";
+            // 
+            // txt_tipo
+            // 
+            this.txt_tipo.Location = new System.Drawing.Point(199, 183);
+            this.txt_tipo.Name = "txt_tipo";
+            this.txt_tipo.Size = new System.Drawing.Size(100, 20);
+            this.txt_tipo.TabIndex = 4;
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Location = new System.Drawing.Point(32, 67);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(100, 20);
+            this.txt_numero.TabIndex = 5;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(171, 265);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(97, 42);
+            this.btn_limpiar.TabIndex = 6;
+            this.btn_limpiar.Text = "NUEVO NUMERO";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // txt_primo
+            // 
+            this.txt_primo.Location = new System.Drawing.Point(199, 218);
+            this.txt_primo.Name = "txt_primo";
+            this.txt_primo.Size = new System.Drawing.Size(100, 20);
+            this.txt_primo.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "EL NUMERO ES PRIMO?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_primo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.txt_tipo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_calcular);
+            this.Name = "Form1";
+            this.Text = "TIPO DE NUMERO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_tipo;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.TextBox txt_primo;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ len.programacion3/ejercicio 1/Tipo numero.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            string tipo;
+        private string Par (int numero)
+        {
+            
+            if (numero % 2 == 0)
+            {
+                tipo = " Es par";
+                
+            }
+                return tipo;
+        }
+        private string Impar(int numero)
+        {
+
+            if (numero % 2 != 0)
+            {
+                tipo = " Es impar";
+
+
+            }
+            return tipo;
+        }
+        int i;
+        private string Primo(int numero)
+        {
+            
+            for (int i = 2; i < numero; i++)
+            {
+                if ((numero % i) == 0)
+                {
+                    
+                    tipo = " no";
+                }
+                else
+                {
+                   tipo = " si";
+                }
+            }
+            
+
+            return tipo;
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            txt_tipo.Text = Par(Convert.ToInt32(txt_numero.Text)).ToString();
+            txt_tipo.Text = Impar(Convert.ToInt32(txt_numero.Text)).ToString();
+            txt_primo.Text = Primo(Convert.ToInt32(txt_numero.Text)).ToString();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_numero.Clear();
+            txt_tipo.Clear();
+        }
+    }
+}
